2f040559ccf06fb0bd5a77f5701db449f8d01074
hxq <495157297@qq.com>
2017-03-27 10:12:01 +0800

    test function for nltk

---
+++ main/nltk/nltk_keywords.py
@@ -0,0 +1,13 @@
+from __future__ import division
+from numpy import *
+def anw():
+    return 'hello2'
+
+def lexical_diversity(text):
+    return len(text) / len(set(text))
+
+
+
+
+
+
--- python_flask.py
+++ python_flask.py
@@ -1,12 +1,44 @@
 from flask import Flask
+from flask import jsonify
+import test
+import main.nltk.nltk_keywords as nltkm
 
 app = Flask(__name__)
 
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/say')
+def say():
+    return test.say()
+
+# @app.route('/anw')
+# def anw():
+#     return nltkm.anw()
+
+@app.route('/keywords')
+def keywords():
+    return jsonify(nltkm.lexical_diversity('hello you hello me'))
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
 
 if __name__ == '__main__':
     app.run()
